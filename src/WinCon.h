19185be12c0d5f7edd80d3e612c2ffde1c600634
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2022-11-08 14:21:58 +0700

    Add initial working code

---
+++ src/WinCon.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <cstdint>
+
+// Based on https://stackoverflow.com/a/55875595/5011111
+
+bool CreateNewConsole(int16_t minLength);
+bool ReleaseConsole();
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
+#include <windows.h>
 #include <iostream>
+#include <conio.h>
+#include "WinCon.h"
 
-int main(int argc, char* argv[])
+INT WINAPI WinMain(
+    HINSTANCE /*hInstance*/,
+    HINSTANCE /*hPrevInstance*/,
+    PSTR      /*lpCmdLine*/,
+    INT       /*cmdShow*/)
 {
-    std::cout << "Hello World!\n";
-}
+    if (CreateNewConsole(1024))
+    {
+        int i;
+
+        // test stdio
+        fprintf(stdout, "Test output to stdout\n");
+        fprintf(stderr, "Test output to stderr\n");
+        fprintf(stdout, "Enter an integer to test stdin: ");
+        scanf("%d", &i);
+        printf("You entered %d\n", i);
+
+        // test iostreams
+        std::cout << "Test output to std::cout" << std::endl;
+        std::cerr << "Test output to std::cerr" << std::endl;
+        std::clog << "Test output to std::clog" << std::endl;
+        std::cout << "Enter an integer to test std::cin: ";
+        std::cin >> i;
+        std::cout << "You entered " << i << std::endl;
+
+        std::cout << std::endl << "Press any key to continue..." << std::endl;
+        _getch();
+
+        ReleaseConsole();
+    }
+
+    return 0;
+};
